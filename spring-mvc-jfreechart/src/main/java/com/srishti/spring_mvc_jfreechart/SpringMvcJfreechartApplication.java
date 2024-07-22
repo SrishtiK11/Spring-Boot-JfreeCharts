6107f34a0f6aa092be081c5d4604d384a8957184
Yuvaprakash N <yuvaprakashnv1999@gmail.com>
2024-07-22 10:55:37 +0530

    Spring-MVC-Jfree Charts

--- spring-mvc-jfreechart/src/main/java/com/srishti/spring_mvc_jfreechart/SpringMvcJfreechartApplication.java
+++ spring-mvc-jfreechart/src/main/java/com/srishti/spring_mvc_jfreechart/SpringMvcJfreechartApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringMvcJfreechartApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringMvcJfreechartApplication.class, args);
 	}
 
